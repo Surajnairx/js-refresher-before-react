c2d47d66bccc21931c60fe1e04c6f08ea1e6c034
Suraj Nair <surajnair9698@gmail.com>
2025-09-18 20:30:37 +0530

    feat: operators

---
+++ basics/operators.js
@@ -0,0 +1,11 @@
+// Operators
+let a = 10,
+  b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Comparison:", a > b);
+console.log("Logical AND:", a > 5 && b > 1);
