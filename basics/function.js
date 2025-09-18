ee9176fed3dbb00525ebcc46bafe66d6890f0f2f
Suraj Nair <surajnair9698@gmail.com>
2025-09-18 20:31:17 +0530

    feat:functions

---
+++ basics/function.js
@@ -0,0 +1,9 @@
+// Functions
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+const greetArrow = (name) => `Hi, ${name}!`;
+
+console.log(greet("Suraj"));
+console.log(greetArrow("React Learner"));
