e2e53d9f842379b93161ffc1f8eaed2df46c1900
Suraj Nair <surajnair9698@gmail.com>
2025-09-18 16:07:46 +0530

    feat: data types

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+// Data Types
+let Name = "Suraj"; // string
+let age = 24; // number
+let isDev = true; // boolean
+let skills = ["HTML", "CSS", "JS"]; // array
+let person = { name: "Suraj", role: "Developer" }; // object
+
+console.log(
+  typeof Name,
+  typeof age,
+  typeof isDev,
+  Array.isArray(skills),
+  typeof person
+);
